32cd7d88c160ef697688d68adc8105a2a970133e
biagoncalvesbia <bg1641436@gmail.com>
2025-06-09 09:41:27 -0300

    .

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Olá, Mundooo! Deploy automático funcionando :)"
+    return "Olá, Mundooo! esta funcionando professor mauricio:)"
 
 if __name__ == '__main__':
     app.run()
